73f529de93fd1039d49215c28e5d476f75557378
Kirakosyan01 <albert69kirakosyan@gmail.com>
2024-08-21 19:31:21 +0400

    new

--- js/index.js
+++ js/index.js
@@ -1,37 +1,42 @@
 const buttons = document.querySelectorAll(".sliderControls");
+const sliderContainer = document.querySelector(".SliderContainer")
 const sliderTrack = document.querySelector(".sliderTrack");
 const leftArrow = document.querySelector(".left");
 const rightArrow = document.querySelector(".right");
 
+const slideWidth = 300;
 let maxTranlateX = 0;
-let minTranslateX = -600;
 let currentTranslateX = 0;
 
 
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    const slideWidth = 300;
     
+    const containerWidth = sliderContainer.clientWidth;
+    const contentWidth = sliderTrack.scrollWidth;
+    let minTranslateX = containerWidth - contentWidth;
+    
+    
+
     if (e.target.dataset.name === "left") {
-        if(currentTranslateX === maxTranlateX) {return}
         currentTranslateX += slideWidth;
-    }
+        
+    };
     if (e.target.dataset.name === "right") {
-        if(currentTranslateX === minTranslateX) {return}
         currentTranslateX -= slideWidth;
-    }
+        
+    };
 
     if(currentTranslateX === maxTranlateX){
         leftArrow.style.display = "none";
-    } else {leftArrow.style.display = "flex"}
+    } else {leftArrow.style.display = "flex"};
 
-    if(currentTranslateX === minTranslateX) {
+    if(currentTranslateX <= minTranslateX) {
         rightArrow.style.display = "none";
-    } else {rightArrow.style.display = "flex"}
+    } else {rightArrow.style.display = "flex"};
     
     sliderTrack.style.transform = `translateX(${currentTranslateX}px)`;
-    console.log(sliderTrack.style.transform);
     
   });
 });
