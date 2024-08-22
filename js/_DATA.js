301a59b22a42ff783833b44f6c318af11c73f857
Kirakosyan01 <albert69kirakosyan@gmail.com>
2024-08-22 21:48:54 +0400

    new

---
+++ js/_DATA.js
@@ -0,0 +1,38 @@
+export const PRODUCTS_DATA = [
+    {
+        id:0,
+        img: "./assets/images/image 8.png",
+        name: "Peperomia Ginny",
+        price: 25,
+    },
+    {
+        id: 1,
+        img: "./assets/images/image 9.png",
+        name: "bird's Nest Fern",
+        price: 45,
+    },
+    {
+        id: 2,
+        img: "./assets/images/image 10.png",
+        name: "Large Majesty Palm",
+        price: 52,
+    },
+    {
+        id: 3,
+        img: "./assets/images/image 11.png",
+        name: "Pet Friendly Plant",
+        price: 30,
+    },
+    {
+        id: 4,
+        img: "./assets/images/image 8.png",
+        name: "Duo Friendly Plant",
+        price: 52,
+    },
+    {
+        id: 5,
+        img: "./assets/images/image 9.png",
+        name: "Nest Fern",
+        price: 65,
+    },
+];
--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+import { PRODUCTS_DATA } from "./_DATA.js";
+
+// Slider
+
 const buttons = document.querySelectorAll(".sliderControls");
 const sliderContainer = document.querySelector(".SliderContainer")
 const sliderTrack = document.querySelector(".sliderTrack");
@@ -7,8 +11,6 @@ const rightArrow = document.querySelector(".right");
 const slideWidth = 300;
 let maxTranlateX = 0;
 let currentTranslateX = 0;
-
-
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -40,3 +42,69 @@ buttons.forEach((button) => {
     
   });
 });
+
+// Cart
+
+let localCart = PRODUCTS_DATA;
+const cartLink = document.querySelector(".userBasket");
+const addButton = document.querySelectorAll(".productBuyButton");
+const aside = document.getElementsByTagName("aside")[0];
+let isCartOpen = false;
+
+aside.style.transition = "opacity 250ms ease-in-out, transform 250ms ease-in-out";
+aside.style.transform = "translateX(260px)";
+
+function handleOpenCart(e) {
+  if(isCartOpen) {
+    aside.style.transform = "translateX(0px)";
+    aside.style.opacity = "1";
+  } else {
+    aside.style.transform = "translateX(260px)";
+    aside.style.opacity = "0";
+  }
+
+  isCartOpen = !isCartOpen;
+}
+
+cartLink.addEventListener("click", handleOpenCart);
+
+addButton.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const productCard = btn.closest('.productCard');
+    const producId = productCard.id;
+
+    const cartUl = document.createElement("ul");
+    const cartLi1 = document.createElement("li");
+    const cartLi2 = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    const productImg = document.createElement("img");
+    productImg.className = "productImg";
+    const trashImg = document.createElement("img");
+    trashImg.src = "./assets/icons/trashCan.webp";
+    trashImg.className = "trashImg";
+
+
+    localCart.forEach((obj) => {
+      if (obj.id == producId) {
+        cartUl.textContent = obj.name;
+        productImg.src = obj.img;
+        cartLi1.append(productImg);
+        cartLi2.textContent = `$${obj.price}`;
+
+        deleteBtn.append(trashImg);
+        deleteBtn.style.marginLeft = "15px";
+        deleteBtn.addEventListener("click", () => {
+          cartUl.remove();
+        });
+
+        cartLi2.append(deleteBtn);
+
+        cartUl.append(cartLi1);
+        cartUl.append(cartLi2);
+        aside.append(cartUl);
+      } else {return}
+    })
+    
+    
+  })
+})
